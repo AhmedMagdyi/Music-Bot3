f5c7cbd10fd0bb93e6cd6592c904843578ae3bef
adadadadadfgfgfgfgfg <43391576+AhmedMagdyi@users.noreply.github.com>
2019-01-26 20:28:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ const prefix = '+'
 
  client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("GMZN Host", {type: 'PLAYING'});
+  client.user.setActivity("GMZN Host | +play", {type: 'PLAYING'});
 });
  
  
